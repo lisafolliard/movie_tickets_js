f77227b9f2044a8b4dadb5d520afe8bcd77814fc
Alisa Colon & Lisa Folliard <student@epicodus.com>
2015-09-15 16:43:26 -0700

    change the layout and form

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,31 @@ Movie.prototype.fullMovie = function() {
 function Ticket(age) {
   this.age = age
 }
+
+function resetFields(){
+  $("input#new-movie-title").val("");
+  $("input#new-movie-time").val("");
+  $("div.hide-form").not(':first').remove();
+}
+
+
+$(document).ready(function() {
+  $("form#new-movie").submit(function(event) {
+    var inputtedMovieTitle = $("input#new-movie-title").val();
+    var inputtedMovieTime = $("input#new-movie-time").val();
+
+    var newMovie = new Movie(inputtedMovieTitle, inputtedMovieTime);
+
+    $("ul#movies").append("<li><span class='movie'>" + newMovie.fullMovie() + "</span></li>");
+
+    $(".movie").last().click(function() {
+      $("#show-movie h2").text("No Movies for You");
+      $("#show-movie").show();
+
+      });
+
+
+    $(resetFields());
+    event.preventDefault();
+  });
+});
